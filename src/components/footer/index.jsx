4d8217a9d385a659603ff9772fe896d311922593
AtmegaBuzz <swapnilshinde9382@gmail.com>
2022-09-17 22:53:19 +0530

    footer fixes

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,16 +1,43 @@
 import React from "react";
 import styles from "./index.module.css";
 import { BsGithub,BsYoutube,BsLinkedin } from 'react-icons/bs';
-import {SiDiscord} from 'react-icons/si';
-import {IoMdMail} from 'react-icons/io';
-import {HiLocationMarker} from 'react-icons/hi'
 import { NavLink } from "react-router-dom";
 import {endpoints} from "../../App";
+
+import {SiDiscord} from 'react-icons/si';
+import {IoMdMail} from 'react-icons/io';
+import {HiLocationMarker} from 'react-icons/hi';
+import {GrInstagram} from 'react-icons/gr';
+import {FaFacebookF} from 'react-icons/fa'
 
 export default function Footer(){
 
     const discordAction = ()=>{
         window.open("https://discord.gg/vX3DDTSt","_blank");
+    }
+
+    const socialAction = (handle)=>{
+
+        switch(handle){
+
+            case "github":
+                window.open("https://github.com/GDSC-NBNSSOE","_blank");
+                break;
+            case "youtube":
+                window.open("https://youtube.com/nbnssoe","_blank");
+                break;
+            case "linkedin":
+                window.open("https://www.linkedin.com/in/google-developer-students-club-nbnssoe/","_blank");
+                break;
+            case "instagram":
+                window.open("https://www.instagram.com/gdsc_nbnssoe/","_blank");
+                break;
+            case "facebook":
+                window.open("https://www.facebook.com/Google-Developer-Students-Club-Nbnssoe-102706765893775","_blank");
+                break;
+            default:
+                break; 
+        }
     }
 
     return (
@@ -33,12 +60,14 @@ export default function Footer(){
             </div>
             <div className={styles.right}>
                 <p>Contact Us</p>
-                <p> <IoMdMail/> gdsc@gmail.com</p>
-                <p> <HiLocationMarker/> Punjab, jjkdasjd, WB, 8989, India</p>
+                <p> <IoMdMail/>nbnssoe.gdsc@gmail.com</p>
+                <p> <HiLocationMarker/>NBNSSOE, Pune, Maharashtra 411041, India</p>
                 <div>
-                    <BsGithub/>
-                    <BsYoutube/>
-                    <BsLinkedin/>
+                    <BsGithub onClick={()=>{socialAction("github")}}/>
+                    <BsYoutube onClick={()=>{socialAction("youtube")}}/>
+                    <BsLinkedin onClick={()=>{socialAction("linkedin")}}/>
+                    <GrInstagram onClick={()=>{socialAction("instagram")}}/>
+                    <FaFacebookF onClick={()=>{socialAction("facebook")}}/>
                 </div>
             </div>
         </div>
