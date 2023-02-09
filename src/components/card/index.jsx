39816ad7aaa5592fdd50ebddd03a9abf66a06172
kiritocode1 <kathawalearyan9@gmail.com>
2023-02-09 10:58:56 +0530

    Rusty anim added , unoptimised

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,32 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 
+import { useSpring , animated as a  } from "@react-spring/web";
 
 import { Card, Col, Text } from "@nextui-org/react";
 
 
-const Cardy = ({heading , description , icon}) => (
-<div className="w-[35rem]">
-<Card isHoverable isPressable >
-    <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-      <Col>
-        <Text size={40} weight="bold" transform="uppercase" color="$black500" className="  hover:visible">
-          {heading}
-        </Text>
-        <Text h4 color="black">
-          {description}
-        </Text>
-      </Col>
-    </Card.Header>
-    <Card.Image
-      src={icon}
-      objectFit="fill"
-      width="100%"
-      height={340}
-        alt="Card image background"
-        className="hover:blur-lg"
-    />
-  </Card>
-</div>
-);
+const Cardy = ({ heading, description, icon }) => {
+  const [isHoverActive, setisHoverActive] = useState(false); 
+  const propsMainText = useSpring({
+    to: {
+      opacity: isHoverActive ? 1 : 0
+    }
+  }); 
+  const propsMainDesc = useSpring({
+        to: {
+      opacity: isHoverActive ? 1 : 0
+    }, 
+    config: {
+      delay: 4000, 
+      duration: 500
+    }
+  }); 
+  return(
+    <div className="w-[35rem]" onMouseEnter={() => setisHoverActive(true)} onMouseLeave={()=>setisHoverActive(false)}>
+      <Card isHoverable isPressable  >
+        <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+          <Col>
+            <a.div style={propsMainText} className="text-5xl font-extrabold font-sans">
+              {heading}
+            </a.div>
+            <a.div style={propsMainDesc} className="text-3xl font-bold">
+              {description}
+            </a.div>
+          </Col>
+        </Card.Header>
+        <Card.Image
+          src={icon}
+          objectFit="fill"
+          width="100%"
+          height={340}
+          alt="Card image background"
+          className={`${isHoverActive?"blur-lg":""}`}
+        />
+      </Card>
+    </div>
+
+  )
+};
 
 export default Cardy; 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -55,8 +55,6 @@ export default function Home(){
                             })
                         }
                     </div>
-
-
             </div>
     )
 }
